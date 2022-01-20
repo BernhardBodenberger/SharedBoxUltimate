d8a4740fb6b44a5dc66892721109f7c6a7d22f72
lontsipennserge <46938117+lontsipennserge@users.noreply.github.com>
2022-01-20 01:36:15 +0100

    Add files via upload

--- newDirectory.java
+++ newDirectory.java
@@ -1,93 +1,33 @@
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.*;
 
 public class newDirectory {
-/*	
-	public void createUserDir(String dirName) throws IOException{
-	    //File homeDir = new File(System.getProperty("user.home"));
-		
-	    String path = "/home/lontsipenn/eclipse-workspace";
-	    File dir = new File(path, dirName);
-	    boolean isCreated = dir.mkdirs();
-	    if(isCreated) {
-	    	System.out.println("Directory created");
-	    } else if (dir.exists()){
-	    	throw new IOException(dir.getAbsolutePath());
-	    } else {
-	    	throw new IOException("File could not be created!!!");
-	    }
-	}
-*/
-	public void renameUserDir(String dirName) throws IOException {
-		//String path = "/home/lontsipenn/Destop/java_SE";
-		File homeDir = new File(System.getProperty("user.home"));
-		File dir = new File(homeDir,dirName);
-		boolean isCreated = dir.mkdirs();
-		
-		if (isCreated) {
-			System.out.println("Directory has been created!");
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter new name of directory");
-			String new_name = sc.nextLine();
-			File newDir = new File(new_name);
-			//File newDir = new File(dir.getParent() + "\\" + new_name);
-			if (dir.renameTo(newDir)) {
-				System.out.println("Directory renamed succesfully");
-			} else {
-				System.out.println("Unsuccesful attempt to rename");
-			}
-		}else {
-			throw new IOException("File could not be created!!!");
+	
+	void my_deleteDirectory(Path path) throws IOException {
+		  Files.walk(path)
+		    .sorted(Comparator.reverseOrder())
+		    .map(Path::toFile)
+		    .forEach(File::delete);
 		}
+	void renameDirectory(File file) {
 		
+		System.out.println("Enter new name! \n");
+		Scanner sc = new Scanner(System.in);
+		String sc_input = sc.next();
+		File newName = new File(file.getParent() + "//" + sc_input);
+		file.renameTo(newName);
 	}
 	
-	public void deleteDirectory(File file) throws IOException{
-		if(file.isDirectory()) {
-			File[] entries = file.listFiles();
-			if(entries != null) {
-				for (File entry : entries) {
-					deleteDirectory(entry);
-				}
-			}
-		}
-		if (!file.delete()) {
-			throw new IOException("Failed to delete " + file);
-		}
-	}
 	
-	public static boolean deleteDir(File dir) {
-		File[] files = dir.listFiles();
-		if(files != null) {
-			for(File file : files) {
-				if(file.isDirectory()) {
-					deleteDir(file);
-				} else {
-					file.delete();
-				}
-			}
-		}
-		return dir.delete();
-	}
-
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String path = "/home/lontsipenn/Desktop/java_SE/new_folder";
-		//File homeDir = new File(System.getProperty("user.home"));
-		File dir = new File(path);
-		boolean iscreated = dir.mkdirs();
-		if (iscreated) {	
-			try {
-				new newDirectory().deleteDirectory(dir);
-			}catch(IOException e) {
-				System.err.println("this " + e.getMessage());
-			}
-		}
-	}	
-
+		
+		File oldname = new File("/path/to/folder/folder");		
+		new newDirectory().renameDirectory(oldname);
+	}
 }
-
 
 
 
