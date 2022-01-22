f98b839d7b598dacb68c87b57de72b0916adab3d
JayMcMingle <jusliebig@uni-potsdam.de>
2022-01-22 14:18:03 +0100

    Update mainGUI.java

--- mainGUI.java
+++ mainGUI.java
@@ -8,8 +8,23 @@ public class mainGUI extends JFrame implements ActionListener {
 	
 	// Creates a new File instance by converting the given pathname string
     // into an abstract pathname
-	File Currend = new File("D:\\Program Files");
-	JFrame frame = null;
+	//File Currend = new File("D:\\Program Files");
+	File CurrendWindow;
+	File AblageGUI;
+	
+	public mainGUI(String s, File Currend, File Ablage) {
+		super(s);
+		CurrendWindow = Currend;
+		AblageGUI = Ablage;
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setSize(500,500);
+		this.setLocation(80,80);
+		this.getContentPane().setBackground(Color.white); 
+		this.setLayout(new FlowLayout());
+		JButton b1 = new JButton("test"); 
+		b1.addActionListener(this);
+		this.getContentPane().add(b1);
+	}
 	
 	public void PopuoMenu(File file) {
 		final JPopupMenu menu = new JPopupMenu("Menu");
@@ -26,49 +41,51 @@ public class mainGUI extends JFrame implements ActionListener {
 	}
 	public void listFiles() {
 		//create new frame
-		frame = new JFrame("Auflistung von Datein");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(500,500);
-		frame.setLocation(80,80);
-		frame.getContentPane().setBackground(Color.white); 
-		frame.setLayout(new FlowLayout());
-		JButton b1 = new JButton("test"); 
-		b1.addActionListener(this);
-		frame.getContentPane().add(b1);
+		
+		
 		
 		
 		String[] pathnames;
 
 
         // Populates the array with names of files and directories
-        pathnames = Currend.list();
+        pathnames = CurrendWindow.list();
 
         //** go through the array
         for (int i = 0; i < pathnames.length; ++i) {
             //create buttons for each element in pathnames
             JButton btn = new JButton(pathnames[i]);
             btn.addActionListener(this);
-            frame.getContentPane().add(btn);
+            this.getContentPane().add(btn);
            
         }
-        frame.setVisible(true);
+        
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 mainGUI gui = new mainGUI();
-		 gui.listFiles();
+		mainGUI frame = new mainGUI("Auflistung von Datein", new File("D:\\Program Files"), null); 
+		
+		frame.listFiles();
+		frame.setVisible(true);
 		 
 	}
 	public void actionPerformed(ActionEvent ev) {
 		
 		JButton test = new JButton();
 		test = (JButton) ev.getSource();
-		frame.getContentPane().add(new JLabel(Currend.getAbsolutePath() + "\\"+ test.getText() ));
+		this.getContentPane().add(new JLabel(CurrendWindow.getAbsolutePath() + "\\"+ test.getText() ));
 		//File Mover = new File(Currend.getAbsolutePath() + "\\"+ test.getText() );
-		//Currend = Mover;
 		
-		frame.validate();
+		File Mover = new File(CurrendWindow.getAbsolutePath() + "\\"+ test.getText());
+		if(Mover.isDirectory()) {
+			CurrendWindow = Mover;
+			mainGUI neu = new mainGUI("test", CurrendWindow, AblageGUI);
+			neu.listFiles();
+			neu.setVisible(true);
+			this.dispose();
+			this.validate();
+		}
 		
 		
 		
