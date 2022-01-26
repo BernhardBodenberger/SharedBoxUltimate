e88bd72a4df64cd644871b4af56c841b314a7dcd
BernhardBodenberger <97947946+BernhardBodenberger@users.noreply.github.com>
2022-01-26 13:17:25 +0100

    Update OpenWindow.java

--- srcForDocumentation/OpenWindow.java
+++ srcForDocumentation/OpenWindow.java
@@ -8,7 +8,13 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
+/**
+ * Klasse, welche je nachdem welche Datei-Operation ausgewählt wurde, ein Fenster zum eingeben von Dateipfaden 
+ * oder Dateinamen öffnet
+ * @author SE1 Projektgruppe 7
+ */
 public class OpenWindow extends JFrame implements ActionListener {
+	//Deklaration der Klassenvariablen
 	boolean rename;
 	boolean move;
 	boolean replace;
@@ -21,7 +27,15 @@ public class OpenWindow extends JFrame implements ActionListener {
 	static JButton pathButton = new JButton("Submit");
 	static JTextField textField = new JTextField();
 	static String path;
-
+	
+	/**
+	 * Konstruktor mit boolean Werten für ausgewähle Datei-Operationen
+	 * @param rename: falls umbenennen ausgewählt wird, rename=true
+	 * @param move: falls verschieben ausgewählt wird, move=true
+	 * @param replace: falls ersetzen ausgewählt wird, replace=true
+	 * @param upload: falls upload ausgewählt wird, upload=true
+	 * @param download: falls download ausgewählt wird, download=true
+	 */
 	OpenWindow(boolean rename, boolean move, boolean replace, boolean upload, boolean download) {
 		this.rename = rename;
 		this.move = move;
@@ -43,6 +57,8 @@ public class OpenWindow extends JFrame implements ActionListener {
 		if (download) {
 			label.setText("Enter the path to where you want to download the file");
 		}
+		
+		// Layout und hinzufügen der Komponenten zum Frame
 		textField.setPreferredSize(new Dimension(350, 40));
 		pathButton.addActionListener(this);
 		label.setHorizontalAlignment(JLabel.LEFT);
@@ -57,24 +73,25 @@ public class OpenWindow extends JFrame implements ActionListener {
 		frame.pack();
 
 	}
-
+	/**
+	 * Methode, welche das Actionevent e auswertet
+	 */
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		if (e.getSource() == pathButton && move == true) {
-			path = textField.getText() + "/" + GUI2.file.name;
-			GUI2.file.move(path);
-			frame.dispose();
+	public void actionPerformed(ActionEvent e) {	
+		if (e.getSource() == pathButton && move == true) { //Falls pathButton gedrückt wurde und move ausgewählt wurde
+			path = textField.getText() + "/" + GUI2.file.name; //Speichert den Pfad der ausgewählten Datei in path
+			GUI2.file.move(path); // Führt die move Methode auf file mit dem Parameter path aus
+			frame.dispose(); //Schließt das Fenster zum eingeben des Pfades
 		}
-		if (e.getSource() == pathButton && rename == true) {
-			path = textField.getText();
-			GUI2.file.rename(path);
-			frame.dispose();
+		if (e.getSource() == pathButton && rename == true) { //Falls pathButton gedrückt wurde und rename ausgewählt wurde
+			path = textField.getText(); //Speichert den Pfad der ausgewählten Datei in path
+			GUI2.file.rename(path); // Führt die rename Methode auf file mit dem Parameter path aus
+			frame.dispose(); //Schließt das Fenster zum eingeben des Pfades
 		}
-		if (e.getSource() == pathButton && replace == true) {
-			path = textField.getText();
-			GUI2.file.replace(path);
-			frame.dispose();
+		if (e.getSource() == pathButton && replace == true) { //Falls pathButton gedrückt wurde und replace ausgewählt wurde
+			path = textField.getText(); //Speichert den Pfad der ausgewählten Datei in path
+			GUI2.file.replace(path); // Führt die replace Methode auf file mit dem Parameter path aus
+			frame.dispose(); //Schließt das Fenster zum eingeben des Pfades
 		}
 		/*if (e.getSource() == pathButton && upload == true) {
 			path = textField.getText();
