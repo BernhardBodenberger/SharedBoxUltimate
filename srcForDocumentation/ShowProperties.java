fcdd34938d6e7584d4c19f6616269a5fbd771c30
BernhardBodenberger <97947946+BernhardBodenberger@users.noreply.github.com>
2022-01-26 13:21:13 +0100

    Update ShowProperties.java

--- srcForDocumentation/ShowProperties.java
+++ srcForDocumentation/ShowProperties.java
@@ -1,9 +1,17 @@
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
-
+/**
+ * Kleine Klasse zur Erstellung eines JFrames, welche die Eigenschaften einer Datei bzw. eines Verzeichnisses
+ * anzeigt
+ * @author SE1 Projektgruppe 7
+ */
 public class ShowProperties extends JFrame {
+	//Deklaration von Klassenvariablen
 	String properties;
-
+	/**
+	 * Konstruktor der ShowProperties Klasse
+	 * @param properties: String, welcher die Eigenschaften einer Datei bzw. eines Verzeichnisses speichert.
+	 */
 	ShowProperties(String properties) {
 		this.properties = properties;
 		JFrame frame = new JFrame("Properties of the file");
